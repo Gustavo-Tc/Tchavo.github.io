b79d92317bf403d3d6828c175367afe32e6bcd48
Gustavo-Tc <153388051+Gustavo-Tc@users.noreply.github.com>
2024-06-30 01:24:18 -0300

    Site formulado
    Site base já feito

--- script.js
+++ script.js
@@ -27,82 +27,142 @@ class Vector extends Array {
     add(other) {
       return this.map((e, i) => e + other[i]);
     }
+
   }
 
-  
+function GetFront(_className){
+    let bake = document.getElementsByClassName(_className)[0];
+
+    return Number(bake.value);
+}
+
+function SetFront(_className, _strings){
+    let bake = document.getElementById(_className);
+
+    let result = "";
+    for(let i = 0; i < _strings.length; i++){
+        result += _strings[i] + "  ";
+    }
+
+    bake.innerHTML = result;
+
+}
+
+function GetAngleFront(_className){
+	let inputDegrees = GetFront(_className + "graus");
+	let inputMinutes = GetFront(_className + "minutos");
+	let inputSeconds = GetFront(_className + "segundos");
+
+    return new Angle(inputDegrees, inputMinutes, inputSeconds);
+}
+
 
 function Start(){
+    Execute();
+}
+
+
+
+
+function Execute(){
     console.log(document.title + " has loaded")
 
-    let _Angulos = new Array(new Angle(285,56,3), new Angle(316,20,35), new Angle(297,44,7));
+    let _Angulos = new Array(GetAngleFront("horizontal1"), GetAngleFront("horizontal2"), GetAngleFront("horizontal3"));
+    //let _Angulos = new Array(new Angle(285,56,3), new Angle(316,20,35), new Angle(297,44,7));
+    //_Angulos.forEach(function (element){ console.log(element); })
+    let _Norte = GetAngleFront("norte");
+
     let _Vertices = 3;
 
-    let _Distances = new Array(29.85398235, 15.99803481, 52.45689655);
+    let _Distances = new Array(GetFront("distancia1"), GetFront("distancia2"), GetFront("distancia3"));
 
-    let _Perímetro = 0;
-    _Distances.forEach(element => {
-        _Perímetro += element;
-    });
+    let _Perímetro = 0; _Distances.forEach(element => {  _Perímetro += element;  });
     //console.log(_Perímetro);
 
     let _SomatórioAngular = CalculoSomatórioAngular(_Angulos);
+    SetFront("somatorioangular", new Array("Somatório Angular:", angleToText(_SomatórioAngular)));
     //console.log( _SomatórioAngular);
 
     let _ExatoAngular = sum(SomatórioAngularExternos(_Vertices));
+    SetFront("somatorioangulardevido", new Array("Somatório Angular Devido:", angleToText(_ExatoAngular)));
     //console.log( _ExatoAngular);
 
     let _ErroAngular = ErroAngular(_SomatórioAngular, _ExatoAngular);
+    SetFront("erroangularcometido", new Array("Erro Angular Cometido:", angleToText(_ErroAngular)));
+
     //console.log(_ErroAngular);
 
     let _TolerânciaAngular = TolerânciaAngular(30, _Vertices);
+    SetFront("erroangularmaximopermitido", new Array("Erro Angular Máximo Permitido:", angleToText(_TolerânciaAngular)));
+
     //console.log(_TolerânciaAngular);
 
     if(_ErroAngular.AngleInt >= _TolerânciaAngular.AngleInt){
+        SetFront("testedeerroangular", new Array("Desaprovado! ultrapassou limite de erro!"));
         console.log("Desaprovado! ultrapassou limite de erro!"); return;
     }
-    else{ console.log("Passou no teste do limite de erro");}
+    else{ 
+        SetFront("testedeerroangular", new Array("Passou no teste do limite de erro, continuando operações"));
+        console.log("Passou no teste do limite de erro");
+    }
 
     let _CorreçãoPorVértice = CorreçãoVértice(_ErroAngular, _Vertices);
+    SetFront("correçaonecessaria", new Array("Correção Necessária:", angleToText(_CorreçãoPorVértice)));
+
     //console.log(_CorreçãoPorVértice);
 
     //console.log(_Angulos[0]);
 
     let _AngulosCorrigidos = CorreçãoDosAngulos(_Angulos, _CorreçãoPorVértice);
+    SetFront("anguloscorrigidos", new Array("Angulos Corrigidos:", "Ângulo 1 : " + angleToText(_AngulosCorrigidos[0]), "   Ângulo 2 : " + angleToText(_AngulosCorrigidos[1]), "   Ângulo 3 : " + angleToText(_AngulosCorrigidos[2])));
+
     //console.log(_AngulosCorrigidos[0]);
 
 
-    let _Azimutes = CalculoAzimutes(new Angle(94,53,17), _AngulosCorrigidos);
+    let _Azimutes = CalculoAzimutes(_Norte, _AngulosCorrigidos);
+    SetFront("azimutesdospontos", new Array("Azimutes Dos Pontos: ", " Azimute 1: " + angleToText(_Azimutes[0]), "|| Azimute 2: " + angleToText(_Azimutes[1]), "|| Azimute 3: " + angleToText(_Azimutes[2])));
     //Azimutes.forEach(function (element){ console.log(element); })
 
     let _ProjeçõesRelativas = ProjeçõesRelativas(_Azimutes, _Distances);
+    SetFront("projeçoesrelativas", new Array("Projeções Relativas Dos Pontos: ", " Ponto 1: " + rounded(_ProjeçõesRelativas[0]), "|| Ponto 2: " + rounded(_ProjeçõesRelativas[1]), "|| Ponto 3: " + rounded(_ProjeçõesRelativas[2])));
     //_ProjeçõesRelativas.forEach(function (element){ console.log(element); })
 
     let _SomatórioPosições = SomatórioPosições(_ProjeçõesRelativas);
     //console.log(_SomatórioPosições);
 
     let _ErroLinearTotal = CalculoErroLinearTotal(_SomatórioPosições);
+    SetFront("errolineartotal", new Array("Erro Linear Total: ", round(_ErroLinearTotal, 6)));
+
     //console.log(_ErroLinearTotal);
 
     let _ErroLinearPermitido = ErroLinearPermitido(_Perímetro , 200);
+    SetFront("errolinearmaximo", new Array("Erro Linear Máximo Permitido: ", round(_ErroLinearPermitido, 6)));
+
     //console.log(_ErroLinearPermitido);
     
     if(_ErroLinearTotal > _ErroLinearPermitido){
+        SetFront("testeerrolinear", new Array("Passou no teste do perímetro, pode prosseguir com os cálculos"));
         console.log("Passou no teste do perímetro, pode prosseguir com os cálculos");
     }else{
+        SetFront("testeerrolinear", new Array("Reprovado nos testes! é necessário um novo levantamento em campo!"));
         console.log("Reprovado nos testes! é necessário um novo levantamento em campo!"); return;
     }
     
     let _PositionsCorrected = CorrigirProjeções(_ProjeçõesRelativas, _Distances, _Perímetro);
+    SetFront("projeçoescorrigidas", new Array("Projeções Corrigidas Dos Pontos: ", " Ponto 1: " + rounded(_PositionsCorrected[0],6), "|| Ponto 2: " + rounded(_PositionsCorrected[1],6), "|| Ponto 3: " + rounded(_PositionsCorrected[2],6)));
     //console.log(_PositionsCorrected[0]);
     //_PositionsCorrected.forEach(function (element){ console.log(element); })
 
     let _CoordenadasTotais = CoordenadasTotais(new Vector(100,200), _PositionsCorrected);
+    SetFront("coordenadastotais", new Array("Cordenadas Totais Dos Pontos: ", " Ponto 1: " + rounded(_CoordenadasTotais[0],6), "|| Ponto 2: " + rounded(_CoordenadasTotais[1],6), "|| Ponto 3: " + rounded(_CoordenadasTotais[2],6)))
     //_CoordenadasTotais.forEach(function (element){ console.log(element); })
     
     let _AzimutesCorrigidos = AzimutesCorrigidos(_PositionsCorrected);
+    SetFront("azimutescorrigidos", new Array("Azimutes Corrigidos Dos Pontos: ", " Ponto 1: " + angleToText(_AzimutesCorrigidos[0]), "|| Ponto 2: " + angleToText(_AzimutesCorrigidos[1]), "|| Ponto 3: " + angleToText(_AzimutesCorrigidos[2])));
     //_AzimutesCorrigidos.forEach(function (element){ console.log(element); })
     
     let _LadosCorrigidos = LadosCorrigidos(_PositionsCorrected);
+    SetFront("ladoscorrigidos", new Array("Lados Corrigidos Da Poligonal: ", " D1: " + round(_LadosCorrigidos[0], 6), " D2: " + round(_LadosCorrigidos[1], 6), " D3: " + round(_LadosCorrigidos[2], 6)));
     //_LadosCorrigidos.forEach(function (element){ console.log(element); })
 
 
@@ -317,7 +377,16 @@ function LadosCorrigidos(_CoordenadasTotais){
     return result;
 }
 
+function round(_value , _casas = 4){
+    return Math.round( _value * (10 ** _casas) ) / (10 ** _casas);
+}
 
+function rounded(_value , _casas = 4){
+   let x = round(_value[0], _casas);
+   let y = round(_value[1], _casas);
+
+   return new Vector(x,y);
+}
 
 function toAngle(_value){
     let result = new Angle(0,0,0);
@@ -333,7 +402,7 @@ function toAngle(_value){
 }
 
 function angleToText(_angle){
-    let Text = _angle.angle + "°" + _angle.minutes + "'" + _angle.seconds + "''"
+    let Text = _angle.angle + "°" + _angle.minutes + "'" + _angle.seconds + "''";
     return Text;
 }
 
